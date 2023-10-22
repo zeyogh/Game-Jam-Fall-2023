cb732a3b035215f7e0d2645f46fd9340d0c99861
jhwagner <jhwagner@iastate.edu>
2023-10-21 19:53:35 -0500

    Player movement fully animated
    integrated animations into player movement

--- GameJamFall2023/Assets/RoomPlayerMovement.cs
+++ GameJamFall2023/Assets/RoomPlayerMovement.cs
@@ -8,6 +8,7 @@ public class RoomPlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
 
@@ -16,6 +17,10 @@ public class RoomPlayerMovement : MonoBehaviour
     {
        movement.x = Input.GetAxisRaw("Horizontal");//X Input
         movement.y = Input.GetAxisRaw("Vertical");//Y Input
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
